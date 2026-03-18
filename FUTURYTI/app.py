8b0a6cb3f546765f356d9cb3438b07c4b6f1eb37
Aletxis <alexis.matute.est@tecazuay.edu.ec>
2026-03-18 09:12:15 -0500

    Update app.py

--- FUTURYTI/app.py
+++ FUTURYTI/app.py
@@ -1,4 +1,6 @@
-
+import streamlit as st
+import pandas as pd
+import plotly.express as px
 
 # 1. Configuración de la página
 st.set_page_config(page_title="Control de Ventas - Inmovision", layout="wide")
